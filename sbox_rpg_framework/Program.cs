a476c3d174b327bad81e52f036f40faba61757f9
Tristan Sladek <tristansladek@gmail.com>
2021-07-28 17:10:16 -0500

    Initialize Project

---
+++ sbox_rpg_framework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sbox_rpg_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
